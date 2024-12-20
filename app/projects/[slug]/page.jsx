90dca8be570969a86e6db345cf7f914bf4248bac
sahaln <sahalnurdin@gmail.com>
2024-12-20 18:35:31 +0700

    add next image

--- app/projects/[slug]/page.jsx
+++ app/projects/[slug]/page.jsx
@@ -2,6 +2,7 @@
 
 import Heading from "../../../components/Heading";
 import ShareLinkButton from "../../../components/ShareLinkButton";
+import Image from "next/image"; // Import komponen next/image
 import { getProject, getSlugs } from "../../../lib/projects";
 
 export async function generateStaticParams() {
@@ -23,7 +24,7 @@ export default async function ProjectPage({ params: { slug } }) {
 
   return (
     <>
-      <div className='max-w-screen-sm mx-auto '>
+      <div className='max-w-screen-sm mx-auto'>
         <Heading>{project.title}</Heading>
 
         <div className='flex items-center gap-3 mt-2'>
@@ -31,20 +32,20 @@ export default async function ProjectPage({ params: { slug } }) {
           <ShareLinkButton />
         </div>
 
-        {/* <iframe
-          src='https://www.youtube.com/'
-          className='mx-auto mt-5 mb-5 rounded aspect-auto sm:aspect-video w-[320px] h-[180px] sm:w-[640px] sm:h-[360px]'
-          sandbox='allow-scripts allow-same-origin' // use with caution
-        /> */}
+        {/* Optimized Image Section */}
         <div className='mt-2'>
-          <img
+          <Image
             src={project.image}
             alt={project.title}
-            width='640'
-            height='360'
+            width={640} // Lebar default
+            height={360} // Tinggi default
             className='object-cover w-full h-auto mx-auto mt-5 mb-5 transition duration-300 ease-in-out delay-150 rounded-xl sm:aspect-video hover:-translate-y-1 hover:scale-110'
+            placeholder='blur' // Placeholder blur
+            blurDataURL='/placeholder.jpg' // Tambahkan placeholder base64 (opsional)
+            priority // Gambar utama dimuat lebih awal
           />
         </div>
+
         <article
           dangerouslySetInnerHTML={{ __html: project.body }}
           className='max-w-screen-sm prose text-left prose-slate dark:prose-invert'></article>
